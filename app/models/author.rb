f6f2ee0cc5d8e43f45af059c5087fae52712ddee
kelly_maass <kmaass77627@gmail.com>
2025-05-22 10:41:45 -0500

    done

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,5 @@ class Author < ApplicationRecord
     validates :name, presence: true
     validates :name, uniqueness: true
     validates :phone_number, length: {is: 10}
+   
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
-    validates :title, presence: true
-    validates :content, length: {minimum: 250}
-    validates :summary, length: {maximum: 250}
-    validates :category, inclusion: ["Fiction", "Non-Fiction"]
-    validates :title, exclusion: ["True Facts"]
+   validates :title, presence: true
+   validates :content, length: {minimum: 250}
+   validates :summary, length: {maximum: 250}
+   validates :category, inclusion: ["Fiction", "Non-Fiction"]
+   validates :title, exclusion: ["True Facts"]
+
 end
